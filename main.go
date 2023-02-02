0b9dd04e78080c3a6e8350f70704d2f4cf93e812
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-02-02 14:35:01 +0300

    Variables declaration

--- main.go
+++ main.go
@@ -3,11 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
+	conferenceName := "Go Conference"
 	const conferenceTickets = 50
 	var remainingTickets = 50
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
+
+	var userName string
+	var userTickets int
+
+	userName = "Ian"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets", userName, userTickets)
 
 }
