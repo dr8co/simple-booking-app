79f7e20a875cf942c13ae024ae0fac680a5767b8
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-02-02 15:00:50 +0300

    Handle user inputs

--- main.go
+++ main.go
@@ -10,13 +10,25 @@ func main() {
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int
 
-	// TODO: ask user their name and how many tickets they would like to order.
+	// Ask the user their info and how many tickets they would like to order.
 
-	userName = "Ian"
-	userTickets = 2
-	fmt.Printf("User %v booked %v tickets", userName, userTickets)
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 
 }
