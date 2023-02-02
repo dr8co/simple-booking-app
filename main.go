732b456f83a997378108a4a3d026a40645eba95b
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-02-02 14:18:33 +0300

    Initial project setup

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Println("Welcome to", conferenceName, "booking application")
+	fmt.Println("We have a total of", conferenceTickets, "tickets and", remainingTickets, "are still available.")
+
+}
